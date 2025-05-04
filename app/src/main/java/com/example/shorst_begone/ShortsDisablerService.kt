2ffdd3633e872646770c4fc866776320c541ccfb
Superior-Snacks <joningiwork@gmail.com>
2025-05-04 19:02:12 +0000

    help me

--- app/src/main/java/com/example/shorst_begone/ShortsDisablerService.kt
+++ app/src/main/java/com/example/shorst_begone/ShortsDisablerService.kt
@@ -11,6 +11,9 @@ class ShortsDisablerService : AccessibilityService() {
     private val cooldown = 40 // I cannot make the app crash
     //when the youtube app changes view if it is a short then go back
     override fun onAccessibilityEvent(event: AccessibilityEvent) {
+        Log.d("ShortsGoBacker", "onAccessibilityEvent called")
+        Log.d("ShortsGoBacker", "Event type: ${event.eventType}")
+        Log.d("ShortsGoBacker", "Package name: ${event.packageName}")
         if (event.eventType == AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED &&
             event.packageName == "com.google.android.youtube") {
             Log.d("DEBUG", "a view change")
